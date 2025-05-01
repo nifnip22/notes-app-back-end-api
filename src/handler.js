6cf2c1004e7c1471eca05ef261105f21a27a9b45
nifnip22 <proxigaming742@gmail.com>
2025-05-01 14:45:27 +0800

    feat: modified notes.js, add routes and handler for add new note

---
+++ src/handler.js
@@ -0,0 +1,35 @@
+/* eslint-disable linebreak-style */
+const { nanoid } = require('nanoid');
+const notes = require('./notes');
+
+const addNoteHandler = (request, h) => {
+  const { title, tags, body } = request.payload;
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = {
+    title, tags, body, id, createdAt, updatedAt,
+  };
+
+  notes.push(newNote);
+
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+  if (isSuccess) {
+    return h.response({
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        noteId: id,
+      },
+    }).code(201);
+  }
+
+  return h.response({
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan',
+  }).code(500);
+};
+
+module.exports = { addNoteHandler };
--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: 'localhost'
   });
+
+  server.route(routes);
 
   await server.start();
   console.log('Server berjalan pada %s', server.info.uri);
